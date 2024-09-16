93460c1d95b899186d16a29b74b45eb9a5edf3c1
Usmanteq <usman.teq@gmail.com>
2024-09-16 12:02:45 +0100

    feat: project setup

---
+++ src/sections/LogoTicker.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LogoTicker = () => {
+  return <div>LogoTicker</div>;
+};
---
+++ src/sections/ProductShowcase.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Productshowcase = () => {
+  return <div>Productshowcase</div>;
+};
